e6d91e43234947649c9f9d3e9fcb529c7c025fd1
AnnieKJR <105496124+AnnieKJR@users.noreply.github.com>
2023-09-11 20:50:20 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -33,6 +33,8 @@ int  main(){
     std::cout << "Player 2: ";
    std::cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
    std::cin >> i >> j;
+   i -= 1;
+   j -= 1;
    if (turn == false)
       game[i][j] = 'X';
    else 
